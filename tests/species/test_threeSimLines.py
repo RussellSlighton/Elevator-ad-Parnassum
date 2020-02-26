5daac46c5366914ce9011306b0284859ebcc4ba6
Russell Slighton <russellslighton@gmail.com>
2020-02-26 15:29:00 -0500

    Added tests for new components
    Added tests for new components

---
+++ tests/species/test_threeSimLines.py
@@ -0,0 +1,6 @@
+from src.species.threeSimLines import *
+
+def test_make_three_sim_lines():
+    opt, line = defineThreeSimLines([Int("")],[Int("")], "name", 8)
+    assert len(opt.assertions()) > len(Optimize().assertions()), "Means that the optimiser has some constraints"
+    assert len(line) == 2
